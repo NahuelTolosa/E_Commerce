0ef467997049bef6162189617ec9e5921dfd7240
Nahuel Tolosa <josetolosa98@gmail.com>
2021-11-21 16:28:39 -0300

    Se agregaron los eventos de los botenes de compra.

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,42 @@
+function ReadJSON(json) {
+    for (const product of json) {
+
+        if (jQuery(location).attr('href').includes(`index.html`)) {
+            if (product.isPopular)
+                CreateNodes(product, `${product.image}`);
+        } else
+            CreateNodes(product, `../${product.image}`);
+
+    }
+}
+
 function CreateNodes(product,imgLocation){
-    $('.grid-section').append(
-        `<div class="img-containter">
-                <div class="img-containter__img">
-                    <img src="${imgLocation}" alt="${product.description}">
-                </div>
-                <div class="img-containter__info">
-                    <h4>${product.name}</h4>
-                    <p>$${product.price}</p>
-                </div>
-                <div class="img-container__option">
-                    <button class="button">Ver más</button>
-                    <button class="button buy-button" value="${product.id}">Comprar</button>
-                </div>
-            </div>`
-    );
+    $(()=>{
+        $('.grid-section').append(
+                `<div class="img-containter">
+                        <div class="img-containter__img">
+                            <img src="${imgLocation}" alt="${product.description}">
+                        </div>
+                        <div class="img-containter__info">
+                            <h4>${product.name}</h4>
+                            <p>$${product.price}</p>
+                        </div>
+                        <div class="img-container__option">
+                            <button class="button">Ver más</button>
+                            <button id="${product.id}" class="button buy-button" >Comprar</button>
+                        </div>
+                    </div>`
+            );
+    });
+    
+}
+
+function BuyButtonListener(product) {
+    $(()=>{
+
+        $(`.buy-button`).on('click',(e)=>{
+            console.log(`Agregado ${e.target.id}`);
+        });
+
+    });
 }
