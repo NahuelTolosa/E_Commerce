8a3d7a19ff5b0aed2ca48b5f44acc0bad880d800
Nahuel Tolosa <josetolosa98@gmail.com>
2021-11-23 12:05:32 -0300

    Se comenzó a trabajar con los eventos de los botones en el carrito.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,3 @@
 ReadJSON();
 PrintCart();
+SetActionButtonsOnCart();
--- js/functions.js
+++ js/functions.js
@@ -49,7 +49,6 @@ function BuyButtonListener() {
     $(()=>{
 
         $(`.buy-button`).on('click',(e)=>{
-            // console.log(`Agregado ${e.target.id}`);
             AddToCart(e.target.parentElement.parentElement);
         });
 
@@ -57,7 +56,11 @@ function BuyButtonListener() {
 }
 
 const AddToCart = item => {
+
+    //Primero paso los datos locales al array en caso que los haya
+    LocalStorageToCar();
     
+    //Creo el nuevo elemento que agregaré en el array
     const product = {
         id:          item.querySelector(`.buy-button`).id,
         name:        item.querySelector(`h4`).textContent,
@@ -67,20 +70,29 @@ const AddToCart = item => {
         quantity:    1
     };
 
+    //Si el elemento ya existe hago +1 en la cantidad
     if (cart.hasOwnProperty(product.id)){
         product.quantity = cart[product.id].quantity + 1;
     }
 
-    CartToLocalStorege();
-
+    //Lo agrego
     cart[product.id] = {...product};
 
+    //Paso los datos del array al localSorage
+    CartToLocalStorege();
+    // console.log(`Cart: ${cart}\n\n\nLocal: ${localStorage.getItem('cart')}`);
+
+}
+
+function LocalStorageToCar() {
+    if (!$.isEmptyObject(JSON.parse(localStorage.getItem(`cart`)))){
+        cart = JSON.parse(localStorage.getItem(`cart`));
+    }
 }
 
 function CartToLocalStorege() {
-    localStorage.setItem(`${cart}`, JSON.stringify(cart));
+    localStorage.setItem(`cart`, JSON.stringify(cart));
 }
-
 
 function PrintCart(){
     if (IsOnCartPage())
@@ -101,30 +113,83 @@ function CreateCartNodes() {
 
             $(`.cart-body`).append(`
                 <tr class="cart__row">
-                        <th class="cart-item">${i}</th>
-                        <th class="cart-item">${product.name}</th>
-                        <th class="cart-item">${product.quantity}</th>
-                        <th class="cart-item">
-                            <div class="plus-less-buttons">
-                                <div class="plus-button">
-                                    <span class="iconify" data-icon="ant-design:plus-circle-outlined"></span>
-                                </div>
-                                <div class="less-button">
-                                    <span class="iconify" data-icon="bi:dash-circle"></span>
-                                </div>
+                    <th class="cart-item">${i}</th>
+                    <th class="cart-item">${product.name}</th>
+                    <th class="cart-item">${product.quantity}</th>
+                    <th class="cart-item">$${product.price}</th>
+                    <th class="cart-item">
+                        <div class="plus-less-buttons">
+                            <div class="plus-button" id="${product.id}">
+                                <span class="iconify" id="${product.id}" data-icon="ant-design:plus-circle-outlined"></span>
                             </div>
-                        </th>
-                        <th class="cart-item">$${product.price}</th>
-                        <th class="cart-item"><span class="iconify" data-icon="clarity:trash-line"></span></th>
-                    </tr>
+                            <div class="less-button" id="${product.id}">
+                                <span class="iconify" id="${product.id}" data-icon="bi:dash-circle"></span>
+                            </div>
+                        </div>
+                    </th>
+                    <th class="cart-item"><span id="${product.id}" class="iconify trash-button" data-icon="clarity:trash-line"></span></th>
+                </tr>
             `);
 
         i++;
 
         }
+
     }
+    $(`.cart-body`).append(`
+        <tr class="cart__row">
+            <th class="cart-item"><h5>Total:<h5></th>
+            <th class="cart-item"></th>
+            <th class="cart-item"></th>
+            <th class="cart-item"></th>
+            <th class="cart-item"><h5>$00000<h5></th>
+            <th class="cart-item"></th>
+        </tr>
+    `);
+
 }
 
 function GetIntPrice(price) {
     return price.slice(1,price.lenght);
+}
+
+function SetActionButtonsOnCart() {
+
+    cart = JSON.parse(localStorage.getItem(`cart`));
+    console.log(cart);
+
+    if(IsOnCartPage()){
+        $(`.plus-button`).on("click",(e)=>{
+            IncreaseAmount(e.target.id);
+        });
+
+        $(`.less-button`).on("click", (e) => {
+            DecreaeseAmount(e.target.id);
+        });
+
+        $(`.trash-button`).on("click",(e)=>{
+
+        });
+    }
+    
+}
+
+function IncreaseAmount(id) {
+    cart = JSON.parse(localStorage.getItem(`cart`));
+    cart[id].quantity += 1;
+    CartToLocalStorege();
+}
+
+function DecreaeseAmount(id) {
+    cart = JSON.parse(localStorage.getItem(`cart`));
+    cart[id].quantity -= 1;
+    CartToLocalStorege();
+}
+
+function DeleteProduct() {
+    
+}
+
+function RefreshAmount() {
+    
 }
